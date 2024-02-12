5aee1bd0f420eb6eb709620baa650fc4023f9fb4
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-02-12 14:51:44 -0600

    learning how to add elements

--- creandoElementos.js
+++ creandoElementos.js
@@ -1,90 +1,44 @@
 /*
-	📌 El Document Object Model nos da metodos que podemos utilizar
-	para poder obtener elementos de la página web.
-	- getElementById
-	- children
-	- parentElement
-	- getElementsByTagName
-	- getElementsByClassName
-	- querySelectorAll
-	- querySelector
-	- closest
-	- matches
+	📌 Agregando elementos al DOM
+	
+	Pasos a seguir:
+	1. Crear un elemento del DOM
+	2. Agregarle texto y atributos
+	3. Insertar el elemento dentro del DOM
 */
 
-/*
-	📌 getElementById
-	Nos permite obtener un elemento en base a su atributo id
-*/
-// const contenedor1 = document.getElementById('contenedor1');
-// console.log(contenedor1);
+const agregarCaja = () => {
+  /*
+		📌 1. Creamos el elemento
+		createElement - Recibe como parametro una cadena de texto con la etiqueta que queremos crear.
+	*/
+  const nuevaCaja = document.createElement('div');
 
-/* 
-	📌 children
-	Nos permite obtener los elementos hijo
-*/
-// console.log(contenedor1.children);
+  /*
+		📌 2. Agregamos texto y atributos
+	*/
+  // nuevaCaja.innerHTML = '<b>Nuevo texto!</b>';
+  nuevaCaja.innerText = 'Nueva caja!';
+  // nuevaCaja.setAttribute('id', 'nuevo-id');
+  nuevaCaja.setAttribute('class', 'caja activa');
 
-/*
-	📌 parentElement
-	Nos permite obtener el elemento padre de un elemento
-*/
-// console.log(contenedor1.parentElement);
+  /*
+		📌 3. Agregamos el elemento al DOM
+	*/
+  const contenedor = document.getElementById('contenedor1');
 
-/*
-	📌 getElementsByTagName
-	Nos permite obtener una colección de elementos en base a la etiqueta.
-	Nota: Devuelve una colección HTML. Una colección no es un arreglo.
-*/
-// const divs = document.getElementsByTagName('div');
-// console.log(divs);
+  // .appendChild() - Agrega un elemento al final
+  // contenedor.appendChild(nuevaCaja);
 
-// Las colecciones incluyen algunas propiedades especiales como length
-// console.log(`Tenemos ${divs.length} divs en la página`);
+  /* .insertAdjacentElement() - Nos permite agregar un elemento 
+	Valores:
+		afterbegin - como primer elemento
+		beforebegin - antes del elemento padre
+		beforeend - como ultimo elemento
+		afterend -  despues del elemento padre
+	*/
+  contenedor.insertAdjacentElement('afterbegin', nuevaCaja);
 
-/*
-	📌 getElementsByClassName
-	Nos permite obtener una colección de elementos en base a su clase CSS.
-	Nota: Devuelve una colección HTML.
-*/
-// const contenedores = document.getElementsByClassName('contenedor');
-// console.log(contenedores);
-
-/*
-	📌 querySelector
-	Nos devuelve el primer elemento que coincida con un selector estilo CSS.
-	Nota: Devuelve un nodelist.
-*/
-const primeraCaja = document.querySelector('#contenedor1 .caja');
-console.log(primeraCaja);
-
-/*
-	📌 querySelectorAll
-	Nos permite obtener una colección de elementos en base a un selector estilo CSS.
-	Nota: Devuelve un nodelist. 
-*/
-const cajas = document.querySelectorAll('.contenedor-principal .caja');
-console.log(cajas);
-
-// nodelist tiene un metodo de tipo foreach para recorrer los elementos.
-// cajas.forEach((caja) => {
-//   console.log(caja.innerHTML);
-// });
-
-/* 
-	📌 closest
-	Nos permite buscar de adentro hacia afuera en busca de un elemento
-*/
-// const ultimaCaja = document.querySelector('.caja:last-child');
-// console.log(ultimaCaja.closest('.contenedor-principal'));
-
-/* 
-	📌 Podemos encadenar todos los metodos para obtener elementos.
-*/
-/* 
-	Muchas veces con querySelector resolvemos casi cualquier caso.
-	Pero aun asi tendremos situaciones donde tendremos que encadenar selectores.
-*/
-const contenedor2 = document.getElementById('contenedor2');
-const contenedor2Caja1 = contenedor2.querySelector('.caja');
-console.log(contenedor2Caja1);
+  // .replaceWith() - Nos permite remplazar el elemento por otro
+  // document.querySelector('#contenedor1 .caja').replaceWith(nuevaCaja);
+};
